ffe99f310d0872711b92b34cf1c992a6975e6352
ilnomad <nathanc124@nycstudents.net>
2025-10-24 09:13:11 -0400

    sdfgsfgsg

--- takeanumber.py
+++ takeanumber.py
@@ -1,19 +1,27 @@
 def takeanumber(n):
     lateness=[]
     z=input("TAKE, SERVE, CLOSE, or EOF ")
-    while z!="EOF":
+    lateness.append(z)
+    while not z=="EOF":
         z=input("TAKE, SERVE, CLOSE, or EOF ")
         lateness.append(z)
     a=0
     t=0
     s=0
-    while lateness[a]!="EOF":
+    while not lateness[a]=="EOF":
         if lateness[a]=="TAKE":
             n+=1
             t+=1
-        if lateness[a]=="SERVE":
-            n+=1
+            if n>=999:
+                n=1
+        elif lateness[a]=="SERVE":
             s+=1
-        if lateness[a]=="CLOSE":
+            if n>=999:
+                n=1
+        elif lateness[a]=="CLOSE":
             print(t,t-s,n)
-takeanumber(23)
+            if n>=999:
+                n=1
+            t=s=0
+        a+=1
+takeanumber(990)
---
+++ zyxwvutsrqponmlkjihgfedcba.py
@@ -0,0 +1,20 @@
+import random
+def realgambling100percent():
+    b=0
+    a=False
+
+    while a==False:
+        x=random.randint(1,100)
+        y=random.randint(1,100)
+        z=random.randint(1,100)
+        if x==y==z:
+            print("you got 0.01% chance")
+            a=True
+            print(b)
+            print(x,y,z)
+        else:
+            print("you didnt get it")
+            b+=1
+            print(x,y,z)
+
+realgambling100percent()
