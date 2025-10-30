bbc778df5b705db64f7d4eb4b9a1db4a97ffda4e
ilnomad <nathanc124@nycstudents.net>
2025-10-30 09:02:54 -0400

    hfhgfshgh

--- austensucks.py
+++ austensucks.py
@@ -1,12 +1,12 @@
+import random
 import tkinter
 from tkinter import *
 from tkinter import ttk
 root = Tk()
-frm = ttk.Frame(root, padding=500)
+frm = ttk.Frame(root, padding=400)
 frm.grid()
-ttk.Label(frm, text="text.txt").grid(column=0, row=0)
-ttk.Button(frm, text="implode", command=root.destroy).grid(column=25, row=25)
-btn = ttk.Button(frm,)
-print(btn.configure().keys())
-print(btn.configure(width=10))
+ttk.Label(frm, text="text.txt").grid(column=(random.randint(1,20)), row=(random.randint(1,20)))
+ttk.Button(frm, text="implode", command=root.destroy).grid(column=(random.randint(1,20)), row=(random.randint(1,20)))
+btn = ttk.Button(frm,text="click if austen",command=root.grid(column=(random.randint(1,20)), row=(random.randint(1,20))))
+print(btn)
 root.mainloop()
