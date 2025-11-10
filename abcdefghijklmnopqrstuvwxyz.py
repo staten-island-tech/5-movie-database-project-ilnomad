cc2ef46534e1fa8ee894c00fac6530ab40d6d367
ilnomad <nathanc124@nycstudents.net>
2025-11-10 09:12:27 -0500

    /

--- abcdefghijklmnopqrstuvwxyz.py
+++ abcdefghijklmnopqrstuvwxyz.py
@@ -14,7 +14,10 @@ def yearafter():
     for h in movielist:
         if x<=movielist[z]["year"]:
             print(movielist[z]["title"])
+            a+=1
         z+=1
+    if a==0:
+        print("nothin")
 def yearrange():
     x=int(input("Min "))
     y=int(input("Max "))
@@ -22,31 +25,44 @@ def yearrange():
     for h in movielist:
         if x<=movielist[z]["year"]<=y:
             print(movielist[z]["title"])
+            a+=1
         z+=1
+    if a==0:
+        print("nothin")
 def yeargiven():
     x=int(input("Year given "))
     z=0
     for h in movielist:
         if x==movielist[z]["year"]:
             print(movielist[z]["title"])
+            a+=1
         z+=1
+    if a==0:
+        print("nothin")
 def specificmovie():
     x=input("Movie search ")
     z=0
     for h in movielist:
         if movielist[z]["title"]==x:
             print(movielist[z]["title"])
+            a+=1
         z+=1
+    if a==0:
+        print("nothin")
 def genresearch():
     genres=input("genres ")
     z=0
     i=0
+    a=0
     for h in movielist:
         for h in movielist[z]["genres"]:
             if genres==movielist[z]["genres"][i]:
+                a+=1
                 print(movielist[z]["title"])
             i+=1
         i=0
         z+=1
+    if a==0:
+        print("nothin")
 
 genresearch()
